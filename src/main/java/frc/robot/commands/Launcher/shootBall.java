72b838104ed72f7ccd4c21750fc96657e9dc952b
Great4Geek <67558194+Great4Geek@users.noreply.github.com>
2022-02-09 18:33:54 -0500

    launcher stuff done

--- src/main/java/frc/robot/commands/Launcher/shootBall.java
+++ src/main/java/frc/robot/commands/Launcher/shootBall.java
@@ -28,11 +28,21 @@ public class shootBall extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    m_launcher.setFlyWheelSpeed(1);
+    m_launcher.setPrelaunchWheelSpeed(1);
+
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_launcher.setFlyWheelSpeed(0);
+    m_launcher.setPrelaunchWheelSpeed(0);
+    
+  }
 
   // Returns true when the command should end.
   @Override
