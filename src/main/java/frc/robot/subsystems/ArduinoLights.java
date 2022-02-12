a6d4fce6f8ac78e5f898f95d0be7ae711c2c5f8d
vanden46 <vanden46@gmail.com>
2022-02-11 22:08:47 -0500

    Create ArduinoLights.java
    Created the code for controlling lights using DIO ports on roboRIO
    -Vanden Haviland

---
+++ src/main/java/frc/robot/subsystems/ArduinoLights.java
@@ -0,0 +1,86 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+/*
+Decimal	    Binary(pin/code)			        Action	
+	        DIO7/do0	DIO8/do1	DIO9/do2		
+0	        0	    	0		    0		    Blue Alliance Normal	
+1	        0	    	0	    	1   		Blue Shoot	
+2	        0		    1		    0	    	Blue Climb	
+3	        0	    	1		    1	    	Red Alliance Normal	
+4	        1		    0		    0   		Red Shoot	
+5	        1		    0		    1	    	Red Climb	
+6           1           1           0           Enforcers
+*/
+
+public class ArduinoLights {
+
+    protected boolean alliance;//false for blue alliance, true for red alliance
+    protected DigitalOutput do0;
+    protected DigitalOutput do1;
+    protected DigitalOutput do2;
+
+    public ArduinoLights(int d0, int d1, int d2){
+        //sets the color of the alliance
+        if(DriverStation.getAlliance()==Alliance.Blue){
+            alliance=false;
+        }
+        else{
+            alliance=true;
+        }
+
+        //sets the pins used for boolean communication
+        do0 = new DigitalOutput(d0);
+        do1 = new DigitalOutput(d1);
+        do2 = new DigitalOutput(d2);
+    }
+
+    public void sendNormal(){
+        if(alliance){
+            do0.set(false);
+            do1.set(false);
+            do2.set(false);
+        }
+        else{
+            do0.set(false);
+            do1.set(true);
+            do2.set(true);
+        }
+    }
+
+    public void shoot(){
+        if(alliance){
+            do0.set(false);
+            do1.set(false);
+            do2.set(true);
+        }
+        else{
+            do0.set(true);
+            do1.set(false);
+            do2.set(false);
+        }
+    }
+
+    public void climb()
+    {
+        if(alliance){
+            do0.set(false);
+            do1.set(true);
+            do2.set(false);
+        }
+        else{
+            do0.set(true);
+            do1.set(false);
+            do2.set(true);
+        }
+    }
+
+    public void enforcers(){
+        do0.set(true);
+        do1.set(true);
+        do2.set(false);
+    }
+}
