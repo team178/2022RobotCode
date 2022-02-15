bdb8061a02aaf3b9d73082b8658cbfd8c93a7231
Great4Geek <67558194+Great4Geek@users.noreply.github.com>
2022-02-15 18:29:07 -0500

    We finished climber.java

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,11 +4,71 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+//import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import frc.robot.Constants.ClimberConstants;
+
+
 
 public class Climber extends SubsystemBase {
 
-  public Climber() {}
+  private final WPI_VictorSPX climberMotor = new WPI_VictorSPX(ClimberConstants.kClimberMotorPort);
+  private final WPI_VictorSPX ExtendingArmWinch = new WPI_VictorSPX(ClimberConstants.kExtendingArmWinchPort);
+
+  private final Solenoid HookSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, ClimberConstants.kHookSolenoidPort);
+
+  private final DigitalInput HighlimitSwitch = new DigitalInput(ClimberConstants.kHighLimitSwitchPort);
+  private final DigitalInput LowlimitSwitch = new DigitalInput(ClimberConstants.kLowLimitSwitchPort);
+
+//Ask if You need to use rate for climber motor(with a double supplier)
+//ask if we need to include conditionals for the limit switches to halt the motors when they are at limit levels
+public Climber() {
+
+    climberMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 10);
+    climberMotor.setSensorPhase(true);
+
+    //Is this right???
+    ExtendingArmWinch.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 10);
+    ExtendingArmWinch.setSensorPhase(true);
+
+  }
+
+  public void setClimberMotorSpeed(double speed){
+
+    climberMotor.set(speed);
+
+  }
+
+  public void setExtendingArmWinchSpeed(double speed){
+
+    ExtendingArmWinch.set(speed);
+
+  }
+
+  public void toggleHookSolenoid(){
+
+    HookSolenoid.toggle();
+
+  }
+
+  public void reset() {
+    climberMotor.setSelectedSensorPosition(0);
+    ExtendingArmWinch.setSelectedSensorPosition(0);
+  }
+//Is this right???
+  public double getClimberMotorEncoder() {
+    return climberMotor.getSelectedSensorPosition(0);
+  }
+
+
+
 
   @Override
   public void periodic() {
