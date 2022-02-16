2af2995f7625752da019c8850f4daafee08e3bab
Henry Latt <92739064+blcrdbob3@users.noreply.github.com>
2022-02-16 17:58:26 -0500

    Added climber commands to autoRoutine.java
    Added climber commands to autoRoutine.java, probably did it wrong so requires checking

--- src/main/java/frc/robot/commands/Autonomous/autoRoutine.java
+++ src/main/java/frc/robot/commands/Autonomous/autoRoutine.java
@@ -6,18 +6,31 @@ import frc.robot.commands.DriveTrain.TurnDegrees;
 import frc.robot.commands.Intake.pickUp;
 import frc.robot.commands.Launcher.shootBall;
 import frc.robot.commands.Limelight.aimRange;
+import frc.robot.commands.climber.LowerMast;
+import frc.robot.commands.climber.RaiseMast;
+import frc.robot.commands.climber.ToggleHook;
+import frc.robot.commands.climber.TomahawkDown;
+import frc.robot.commands.climber.TomahawkUp;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.LimeLight;
 import frc.robot.subsystems.Turret.Intake;
 import frc.robot.subsystems.Turret.Launcher;
+import frc.robot.subsystems.Climber;
 
 public class autoRoutine extends SequentialCommandGroup {
     
-    public autoRoutine(DriveTrain drivetrain, Intake intake, Launcher launcher, LimeLight limelight){
+    public autoRoutine(DriveTrain drivetrain, Intake intake, Launcher launcher, Climber climber, LimeLight limelight){
         addCommands(
-            //add commands for the robot to drive to ball (Someone can code this out)
+            //add commands for the robot to drive (More commands to driveTrain will be added once PathFinder is coded)
             new DriveStraight(10, drivetrain),
             new TurnDegrees(.3, 90, drivetrain),
+            
+            //add commands for climber
+            new LowerMast(climber),
+            new RaiseMast(climber),
+            new ToggleHook(climber),
+            new TomahawkDown(climber),
+            new TomahawkUp(climber), 
 
             //add intake command to pick up ball
             new pickUp(intake),
