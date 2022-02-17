2f3e325b47dff0ae5685073315ab787f8ad9ab26
Andy Chen <bobopop787@gmail.com>
2022-02-17 12:23:26 -0500

    Updated ToggleHook.java and added base code

--- src/main/java/frc/robot/commands/climber/ToggleHook.java
+++ src/main/java/frc/robot/commands/climber/ToggleHook.java
@@ -11,4 +11,28 @@ public class ToggleHook extends CommandBase {
         // Use addRequirements() here to declare subsystem dependencies.
         addRequirements(climber);
     }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        m_climber.getHookSolenoid().toggle();
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
