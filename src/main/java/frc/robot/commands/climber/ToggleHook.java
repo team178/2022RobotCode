393f899b5392957be74b745f987dd6273a1cab71
Andy Chen <bobopop787@gmail.com>
2022-02-17 12:06:01 -0500

    Added base code to commands

--- src/main/java/frc/robot/commands/climber/ToggleHook.java
+++ src/main/java/frc/robot/commands/climber/ToggleHook.java
@@ -1,5 +1,14 @@
 package frc.robot.commands.climber;
 
-public class ToggleHook {
-    
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Climber;
+
+public class ToggleHook extends CommandBase {
+    private final Climber m_climber;
+
+    public ToggleHook(Climber climber) {
+        m_climber = climber;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(climber);
+    }
 }
--- src/main/java/frc/robot/commands/climber/TomahawkUp.java
+++ src/main/java/frc/robot/commands/climber/TomahawkUp.java
@@ -1,5 +1,14 @@
 package frc.robot.commands.climber;
 
-public class TomahawkUp {
-    
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Climber;
+
+public class TomahawkUp extends CommandBase {
+    private final Climber m_climber;
+
+    public TomahawkUp(Climber climber) {
+        m_climber = climber;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(climber);
+    }
 }
