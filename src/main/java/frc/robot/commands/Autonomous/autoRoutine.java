3e7082f5c59e0d9ec3c879f637c995abcf7e0c56
Henry Latt <92739064+blcrdbob3@users.noreply.github.com>
2022-02-17 16:08:15 -0500

    Added Arduino commands to autoRoutine.java
    Will require changing later once Arduino is fully fleshed out

--- src/main/java/frc/robot/commands/Autonomous/autoRoutine.java
+++ src/main/java/frc/robot/commands/Autonomous/autoRoutine.java
@@ -11,6 +11,9 @@ import frc.robot.commands.climber.RaiseMast;
 import frc.robot.commands.climber.ToggleHook;
 import frc.robot.commands.climber.TomahawkDown;
 import frc.robot.commands.climber.TomahawkUp;
+import frc.robot.commands.ArduinoLights.Climb;
+import frc.robot.commands.ArduinoLights.SendNormal;
+import frc.robot.commands.ArduinoLights.Shoot;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.LimeLight;
 import frc.robot.subsystems.Turret.Intake;
@@ -19,7 +22,7 @@ import frc.robot.subsystems.Climber;
 
 public class AutoRoutine extends SequentialCommandGroup {
     
-    public AutoRoutine(DriveTrain drivetrain, Intake intake, Launcher launcher, Climber climber, LimeLight limelight){
+    public AutoRoutine(DriveTrain drivetrain, Intake intake, Launcher launcher, Climber climber, Arduino arduino, LimeLight limelight){
         addCommands(
             //add commands for the robot to drive (More commands to driveTrain will be added once PathFinder is coded)
             new DriveStraight(10, drivetrain),
@@ -40,6 +43,12 @@ public class AutoRoutine extends SequentialCommandGroup {
 
             //add launcher command to shoot ball
             new shootBall(launcher)
+            
+            //add arduino commands
+            new ArduinoClimb(arduino, climb);
+            new ArduinoSendNormal(arduino, sendnormal);
+            new ArduinoShoot(arduino, shoot);
+            
         );
     }
 }
