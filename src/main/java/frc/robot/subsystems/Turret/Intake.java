e72b8f506914a263ea81e784ad3a40f6ed9678c8
Henry Latt <92739064+blcrdbob3@users.noreply.github.com>
2022-02-17 15:53:17 -0500

    Intake first draft
    Made intake first draft; requires checking to make sure the intake actually works.

--- src/main/java/frc/robot/subsystems/Turret/Intake.java
+++ src/main/java/frc/robot/subsystems/Turret/Intake.java
@@ -5,10 +5,40 @@
 package frc.robot.subsystems.Turret;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import frc.robot.Constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
+  private final WPI_VictorSPX intakeMotor = new WPI_VictorSPX(IntakeConstants.MOTOR_PORT);
   
-  public Intake() {}
+  public Intake() {
+    intakeMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 10);
+    intakeMotor.setSensorPhase(true);
+  }
+  
+  /**
+   * Sets intakeMotorSpeed to a random int
+  */ 
+  public void setClimberMotorSpeed(double speed) {
+    intakeMotor.set(speed);
+  }
+  
+  /**
+    * Resets position of motor
+  */
+  
+  public void reset() {
+    intakeMotor.setSelectedSensorPosition(0);
+  }
+  
+  /**
+    * Encoder for motor
+  */
+  
+  public double getClimberMotorEncoder() {
+    return intakeMotor.getSelectedSensorPosition(0);
+  }
 
   @Override
   public void periodic() {
