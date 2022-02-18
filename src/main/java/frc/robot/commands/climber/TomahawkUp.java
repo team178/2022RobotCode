946b84c7d5034d4dda588d3654366d4e07ccdb64
Team178Laptop <programming@farmingtonrobotics.org>
2022-02-18 14:24:03 -0500

    Finished base code

--- src/main/java/frc/robot/commands/climber/TomahawkUp.java
+++ src/main/java/frc/robot/commands/climber/TomahawkUp.java
@@ -11,4 +11,30 @@ public class TomahawkUp extends CommandBase {
         // Use addRequirements() here to declare subsystem dependencies.
         addRequirements(climber);
     }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        m_climber.setClimberMotorSpeed(0);
+        m_climber.climberReset();
+    }
+  
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        m_climber.setClimberMotorSpeed(1); //may need to adjust based on motor
+    }
+  
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        m_climber.setClimberMotorSpeed(0);
+        m_climber.climberReset();
+    }
+  
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+      return false;
+    }
 }
