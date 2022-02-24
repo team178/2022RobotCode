e9848a979536ad338db20dd80d54d47425cd4b1f
Great4Geek <67558194+Great4Geek@users.noreply.github.com>
2022-02-24 10:44:37 -0500

    Fixed errors

--- src/main/java/frc/robot/commands/shootBall.java
+++ src/main/java/frc/robot/commands/shootBall.java
@@ -2,13 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.launcher;
+package frc.robot.commands.Launcher;
 
+import frc.robot.subsystems.Turret.Launcher;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.turret.Launcher;
 
 /** An example command that uses an example subsystem. */
-public class ShootBall extends CommandBase {
+public class shootBall extends CommandBase {
   private final Launcher m_launcher;
 
   /**
@@ -16,33 +16,33 @@ public class ShootBall extends CommandBase {
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ShootBall(Launcher launcher) {
+  public shootBall(Launcher launcher) {
     m_launcher = launcher;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(launcher);
+
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    m_launcher.setFlyWheelSpeed(0);
-    m_launcher.setFeedWheelSpeed(0);
-    m_launcher.reset();
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+
     m_launcher.setFlyWheelSpeed(1);
-    m_launcher.setFeedWheelSpeed(1);
+    m_launcher.setPrelaunchWheelSpeed(1);
+
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     m_launcher.setFlyWheelSpeed(0);
-    m_launcher.setFeedWheelSpeed(0);
-    m_launcher.reset();
+    m_launcher.setPrelaunchWheelSpeed(0);
+    
   }
 
   // Returns true when the command should end.
