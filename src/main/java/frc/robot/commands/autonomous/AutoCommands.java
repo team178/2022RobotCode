e9bad7979384e88eaf793cfea77064d5e7362143
Team178Laptop <programming@farmingtonrobotics.org>
2022-02-26 12:56:26 -0500

    Added potential BlueRight pathing

--- src/main/java/frc/robot/commands/autonomous/AutoCommands.java
+++ src/main/java/frc/robot/commands/autonomous/AutoCommands.java
@@ -2,6 +2,8 @@ package frc.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.arduinolights.Shoot;
+import frc.robot.commands.drivetrain.AutoDrive;
+import frc.robot.commands.drivetrain.TurnDegrees;
 import frc.robot.commands.intake.PickUp;
 import frc.robot.commands.launcher.ShootBall;
 import frc.robot.commands.limelight.AimRange;
@@ -12,6 +14,7 @@ import frc.robot.subsystems.turret.Intake;
 import frc.robot.subsystems.turret.Launcher;
 
 public class AutoCommands {
+    //Left, middle, right, from looking from the hub outwards perspectives
     public static SequentialCommandGroup RedLeftAuto;
     public static SequentialCommandGroup RedMiddleAuto;
     public static SequentialCommandGroup RedRightAuto;
@@ -24,7 +27,6 @@ public class AutoCommands {
     public static void init(DriveTrain drivetrain, Intake intake, Launcher launcher, ArduinoLights lights, LimeLight limelight) {
         // TO-DO: need to add pathing commands in group
         RedLeftAuto = new SequentialCommandGroup(
-            //Pathing commands
             new PickUp(intake),
             new AimRange(drivetrain, limelight),
             new ShootBall(launcher),
@@ -64,7 +66,9 @@ public class AutoCommands {
         );
         
         BlueRightAuto = new SequentialCommandGroup(
-            //Pathing commands
+            new AutoDrive(1, 85.67 /* inches?? */, drivetrain),
+            new TurnDegrees(1, 270, drivetrain),
+            new AutoDrive(1, 34.39 /* inches?? */, drivetrain),
             new PickUp(intake),
             new AimRange(drivetrain, limelight),
             new ShootBall(launcher),
