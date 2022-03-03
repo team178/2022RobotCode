801a5b4129236ce6a106fdf2184c71df0a386d5b
Team178Laptop <programming@farmingtonrobotics.org>
2022-03-03 18:01:32 -0500

    Invert Mast limit switch

--- src/main/java/frc/robot/commands/climber/LowerMast.java
+++ src/main/java/frc/robot/commands/climber/LowerMast.java
@@ -34,6 +34,6 @@ public class LowerMast extends CommandBase {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-      return !m_climber.getLowLimitSwitch().get();
+      return m_climber.getLowLimitSwitch().get();
     }
 }
