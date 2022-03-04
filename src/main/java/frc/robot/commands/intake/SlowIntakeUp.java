b7b7ad82fd81a71088e1e5f3a92974bcc17299b9
Great4Geek <67558194+Great4Geek@users.noreply.github.com>
2022-03-04 17:23:02 -0500

    rightTrigger held,intake = slow if up-hold ball in

---
+++ src/main/java/frc/robot/commands/intake/SlowIntakeUp.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.turret.Intake;
+
+/** Pick up balls with the Intake */
+public class SlowIntakeUp extends CommandBase {
+  
+  private final Intake m_intake;
+
+  /**
+   * Creates a new PickUp command.
+   *
+   * @param intake the Intake subsystem.
+   */
+  public SlowIntakeUp(Intake intake) {
+    m_intake = intake;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intake);
+  }
+
+  // Called when the command is initially scheduled. 
+  @Override
+  public void initialize() {
+    // m_intake.close();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (m_intake.isDeployed()) {
+      
+      m_intake.setSpeed(-0.3);
+
+    }
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) throws RuntimeException {
+   
+    m_intake.setSpeed(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
