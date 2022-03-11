75b35d3933e854c28a5fbb00c281aeac1693825e
MASTR_V_BOI <43796295+22chamartyva@users.noreply.github.com>
2022-03-10 22:49:36 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -187,7 +187,7 @@ public class RobotContainer {
     //m_autoChooser.addOption("Right Auto", AutoCommands.RightAuto);
     m_autoChooser.addOption("Modified Range", new ModifiedRange(m_drivetrain, m_limelight, 1.2192));
     m_autoChooser.addOption("Modified Aim", new ModifiedAim(m_drivetrain, m_limelight));
-    m_autoChooser.addOption("Aim and Range", new AimRange(m_drivetrain, m_limelight));
+    m_autoChooser.addOption("Aim and Range", new AimRange(m_drivetrain, m_limelight,1.2192));//number got by looking at limelight distance and pushing it against the hub -.1 meter
     m_autoChooser.addOption("Auto PickUp", new AutoPickUp(m_intake, m_drivetrain, 0.5842));
     m_autoChooser.addOption("Auto Shoot", new AutoShootBall(m_launcher, m_feeder, m_limelight));
 
