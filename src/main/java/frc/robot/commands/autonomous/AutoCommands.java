7c0cc5ae7bc0402541728152c8d089dda0768854
22lejo <22lejo@fpsct.org>
2022-03-10 23:47:21 -0500

    Created a new autocommand called ModifiedGeneralAuto, uses ModifiedRange and ModifiedAim seperately

--- src/main/java/frc/robot/commands/autonomous/AutoCommands.java
+++ src/main/java/frc/robot/commands/autonomous/AutoCommands.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.drivetrain.TurnDegrees;
 import frc.robot.commands.drivetrain.DriveStraight;
 import frc.robot.commands.limelight.AimRange;
+import frc.robot.commands.limelight.ModifiedAim;
+import frc.robot.commands.limelight.ModifiedRange;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.LimeLight;
 import frc.robot.subsystems.turret.Feeder;
@@ -16,6 +18,7 @@ public class AutoCommands {
     public static SequentialCommandGroup MiddleAuto;
     public static SequentialCommandGroup RightAuto;
     public static SequentialCommandGroup GeneralAuto;
+    public static SequentialCommandGroup ModifiedGeneralAuto;
 
     //public static SequentialCommandGroup AutoClimb;
 
@@ -74,7 +77,16 @@ public class AutoCommands {
         GeneralAuto = new SequentialCommandGroup(
             new AimRange(drivetrain, limelight, 1.2192),
             new AutoShootBall(launcher,feeder,limelight),
-            new DriveStraight(-2.54, drivetrain)
+            new DriveStraight(-3.3, drivetrain)
         );
+
+        ModifiedGeneralAuto = new SequentialCommandGroup(
+            new ModifiedRange(drivetrain, limelight, 1.2192),
+            new ModifiedAim(drivetrain, limelight),
+            new AutoShootBall(launcher, feeder, limelight),
+            new DriveStraight(-3.3, drivetrain)
+        );
+
+        
     }
 }
