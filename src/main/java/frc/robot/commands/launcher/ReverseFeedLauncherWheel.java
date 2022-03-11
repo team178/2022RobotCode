c5d2775234428ab7b1acb3a0adc80bce0e1fc48f
Team178Laptop <programming@farmingtonrobotics.org>
2022-03-10 20:06:28 -0500

    Removing speed set from command constructor

--- src/main/java/frc/robot/commands/launcher/ReverseFeedLauncherWheel.java
+++ src/main/java/frc/robot/commands/launcher/ReverseFeedLauncherWheel.java
@@ -26,7 +26,6 @@ public class ReverseFeedLauncherWheel extends CommandBase {
     m_feeder = feeder;
 
     this.speed = LauncherConstants.kLauncherSpeed.getDouble(-0.05);
-    m_feeder.setSpeed(0.05);
 
     addRequirements(launcher);
   }
