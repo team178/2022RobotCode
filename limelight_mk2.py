a2ed5e8f6a08066caf8b11bc35f8141354c8cd63
MASTR_V_BOI <43796295+22chamartyva@users.noreply.github.com>
2022-04-21 17:27:19 -0400

    Limelight Adjustments mk3

--- limelight_mk2.py
+++ limelight_mk2.py
@@ -1,18 +1,18 @@
 #imports used
 import cv2
 import numpy as np
-
+#Written By Varun Chamarty & Jonathan Le
 
 # runPipeline() is called every frame by Limelight's backend.
 def runPipeline(image, llrobot):
     # convert the input image to the HSV color space
     img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    # convert the hsv to a binary image by removing any pixels
+    
     # that do not fall within the following HSV Min/Max values(min(H,S,V), (max(H,S,V)))
     #img_threshold = cv2.inRange(img_hsv, (78, 39, 48), (90, 237, 244))
-    img_threshold = cv2.inRange(img_hsv, (50, 50, 100), (65, 200, 200))
-                                        #dont adjust the first two unless you change the setting on the first page, which are 2,0, 500,500 respectively
-    # find contours in the new binary image
+    img_threshold = cv2.inRange(img_hsv, (50, 100, 100), (120, 260, 200))
+                                        #Make Adjustments here, left side is the minimum values, right side is the maximum values, adjust accordingly (Hue,Saturation,Brightness Value)
+    # find contours in the new image
     contours, _ = cv2.findContours(img_threshold,
     cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -37,11 +37,16 @@ def runPipeline(image, llrobot):
         
         left, top = np.min(listofCoordinates, axis=0)[:2]
        #finding the top left most value within given threshold
+    
         right, bottom = np.max(listofCoordinates, axis=0)[2:]
         #finding the bottom right vaues within given threshold
+        
         returnContour = [[[(left+right)/(2.0),(top+bottom)/(2.0)]]]
        #Find the average of both to find the center of the given rectangle(should work for any size rectangle)
+    
         cv2.rectangle(image, (left, top), (right, bottom), (0, 0,255), 2)
+        #This takes the image, take the two points given to it (leftx,topy) 
+        #and (rightx,bottomy) and draws the rectangle:Numbers are for color(BGR) and line thickness respectively
       
         # record some custom data to send back to the robot
         llpython = [1,x,y,w,h,9,8,7]
